87dc8bd56e34a28137cc1291fc82f5b9f8da05a1
Stanley Delva <juniordelva1@gmail.com>
2025-04-07 17:29:05 -0400

    added renderMoviesCards,cardHtml,initialize, setupAddEventlstener functions

--- app.js
+++ app.js
@@ -5,15 +5,65 @@ import {
     createMovie,
     displayMovies,
 } from "./backend.js";
+
+// Movie class
 import { Movie } from "./movie.js";
 
 const MovieApp = (function () {
-    let five = 5;
+    const allMoviesLinks = document.querySelector(".all-movies-link");
+    const watchedMoviesLinks = document.querySelector(".watched-movies-link");
+    const planToWatchedLink = document.querySelector(".plan-to-watch-link");
+    const moviesSection = document.querySelector(".movies-section");
+    const trackAllMovies = false;
 
-    return {
-        five,
-    };
+    async function init() {
+        setupEventListener();
+        await renderMoviesCards();
+    }
+
+    function setupEventListener() {
+        allMoviesLinks.addEventListener("click", () => {
+            trackAllMovies = true;
+        });
+
+        watchedMoviesLinks.addEventListener("click", () => {
+            trackAllMovies = false;
+        });
+
+        planToWatchedLink.addEventListener("click", () => {
+            trackAllMovies = false;
+        });
+    }
+
+    async function renderMoviesCards() {
+        moviesSection.innerHTML = "";
+
+        if (moviesSection.innerHTML === "") {
+            moviesSection.innerHTML =
+                "<h1 class = 'no-movies-comment'>No Movies</h1>";
+        }
+
+        try {
+            const movieData = await getMovie();
+
+            //load movie into the section
+            moviesSection.innerHTML = movieData
+                .map((movie) => {
+                    return cardHtml(movie);
+                })
+                .join("");
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    function cardHtml(movie) {
+        return `<div> <p>${movie.title}</p> </div>`;
+    }
+
+    return { init };
 })();
-console.log(MovieApp.five);
+// console.log(MovieApp.five);
 displayMovies();
+MovieApp.init();
 // getMovie();
